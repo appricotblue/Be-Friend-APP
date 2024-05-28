66ebf18f4beb40ffe4ebf745284210cf9e9ff0ca
appricotblue <git2@aindriya.com>
2024-05-28 15:06:43 +0530

    bundle id changed

--- ios/BeFriendTests/BeFriendTests.m
+++ ios/BeFriendTests/BeFriendTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNTests : XCTestCase
+@interface befriendTests : XCTestCase
 
 @end
 
-@implementation RNTests
+@implementation befriendTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
