e79181184cbc7130b0a5eb22fea15b52f8cb88b5
appricotblue <git2@aindriya.com>
2024-05-28 16:08:09 +0530

    language translation

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, Button } from 'react-native';
+import { useLanguage } from '../../LanguageContext';
 
 const Home = ({navigation: {navigate}}) => {
+  const { t, changeLanguage } = useLanguage()
   const OnHomePress = () => {
     navigate('cart');
   };
@@ -9,9 +11,14 @@ const Home = ({navigation: {navigate}}) => {
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity onPress={() => OnHomePress()}>
+
         <Text style={styles.text}>Home Screen</Text>
+        <Text>{t('home')}</Text>
+        <Text>{t('welcome')}</Text>
+
         {/*  */}
       </TouchableOpacity>
+    
     </SafeAreaView>
   );
 };
